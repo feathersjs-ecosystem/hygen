dde39f29cd965232131f7ab8e4c8af93243026d5
fratzinger <frederik.schmatz@artesa.de>
2021-12-31 16:26:57 +0100

    test: revert wrong template

--- src/__tests__/metaverse/hygen-templates/_templates/init/self/hello.ts
+++ src/__tests__/metaverse/hygen-templates/_templates/init/self/hello.ts
@@ -1,12 +1,16 @@
 export function render(context: any) {
-  const to = `my_app/template.md`
+  const to = `given/_templates/sample/new/hello.ejs.t`
   const body = `
+---
+to: my_app/template.md
+---
+
 hello metaverse!
 This is your first hygen template.
 
 Learn what it can do here:
 
-https://github.com/jondot/hygen    
+https://github.com/jondot/hygen
 `
 
   return {
